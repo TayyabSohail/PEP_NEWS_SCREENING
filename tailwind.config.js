198e43d1527ce5f1c1a926032cc50c1caf33c780
moinuddinrao <moinrajput594@gmail.com>
2024-06-12 12:15:44 +0500

    style: update tailwind.config.js with theme colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // General colors
+        white: "#FFFFFF",
+        black: "#000000",
+        light_blue: "#F5FAFF",
+        gray: "#6D6D6D",
+        light_gray: "#C4C4C4",
+        blue: "#DEEFFF",
+        pink: "#FBDBFE",
+        red: "#FFC9C9",
+        green: "#CFFFD8",
+
+        // Brand colors,
+        primary: "#085FAB",
+        secondary: "#08ABA6",
+      },
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
