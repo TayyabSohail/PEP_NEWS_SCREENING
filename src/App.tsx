5880e2db8f567acf1a40abcd897a5e6b54129709
moinuddinrao <moinrajput594@gmail.com>
2024-06-12 11:46:07 +0500

    build: add Ant Design dependency and customize antd theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
+import { App as AntdApp, ConfigProvider } from "antd";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <AntdApp>
+      <div className="App">
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#085FAB",
+              colorPrimaryBg: "#E5EDFF",
+              colorBorder: "#085FAB",
+              borderRadius: 6,
+              colorText: "#454545",
+              colorTextSecondary: "#454545",
+            },
+          }}
+        >
+          App
+        </ConfigProvider>
+      </div>
+    </AntdApp>
+  );
 };
