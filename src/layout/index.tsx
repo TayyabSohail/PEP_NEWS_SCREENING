67fc7dd5102e7c287d21f2185d3e1e4538541dd4
moinuddinrao <moinrajput594@gmail.com>
2024-06-14 16:58:29 +0500

    refactor: update layout component style

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,9 +7,9 @@ import { Header } from "../components/Header";
 export const Layout = () => {
   return (
     <Space direction="vertical" className="w-full h-full min-h-screen">
-      <AntdLayout className="w-full h-full bg-light_blue">
-        {/* <Header /> */}
-        <AntdLayout.Content className="h-full flex flex-col gap-y-10">
+      <AntdLayout className="w-full h-full bg-white">
+        <Header />
+        <AntdLayout.Content className="w-full h-full min-h-[93vh] flex flex-col gap-y-10">
           <Outlet />
         </AntdLayout.Content>
       </AntdLayout>
