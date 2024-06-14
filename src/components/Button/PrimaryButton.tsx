25dd77dc5ebec543181bafa018288b69b6b17f51
moinuddinrao <moinrajput594@gmail.com>
2024-06-14 11:34:13 +0500

    chore: update PrimaryButton component styling and importing

--- src/components/Button/PrimaryButton.tsx
+++ src/components/Button/PrimaryButton.tsx
@@ -1,10 +1,14 @@
-
 import { Button, ButtonProps } from "antd";
-export const PrimaryButton = (props : ButtonProps) => {
-  return (
-    <Button {...props} htmlType={props?.htmlType}  onClick={props?.onClick}   className={`font-semibold font-lato mt-3 px-6 py-5 bg-primary text-white rounded-full ${props?.className}`}>
-         {props.children} 
-         </Button>
-  )
-}
 
+export const PrimaryButton = (props: ButtonProps) => {
+  return (
+    <Button
+      {...props}
+      htmlType={props?.htmlType}
+      onClick={props?.onClick}
+      className={`w-fit font-semibold font-lato px-8 py-4 bg-primary text-white rounded-full ${props?.className}`}
+    >
+      {props.children}
+    </Button>
+  );
+};
