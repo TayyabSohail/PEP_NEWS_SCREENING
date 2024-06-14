11e8cc99a77e9e507b5bc4c3270411242048b328
moinuddinrao <moinrajput594@gmail.com>
2024-06-14 11:35:50 +0500

    style: add styles for headings, labels, and disabled state

---
+++ src/assets/styles/index.ts
@@ -0,0 +1,16 @@
+export const styles = {
+  heading1:
+    "m-0 p-0 font-lato font-bold text-[48px] leading-[56px] text-primary",
+  heading2:
+    "m-0 p-0 font-lato font-bold text-[32px] leading-[40px] text-primary",
+  heading3:
+    "m-0 p-0 font-lato font-bold text-[24px] leading-[32px] text-primary",
+  heading4:
+    "m-0 p-0 font-lato font-bold text-[20px] leading-[28px] text-primary",
+  heading5:
+    "m-0 p-0 font-lato font-bold text-[16px] leading-[24px] text-primary",
+
+  label:
+    "!m-0 !p-0 !font-lato !font-bold !text-[16px] !leading-[24px] !text-black",
+  disabled: "!text-white !bg-primary !opacity-50",
+};
