7b9966b12906f0055e31917b01663955e0eb0e86
moinuddinrao <moinrajput594@gmail.com>
2024-06-14 11:36:20 +0500

    refactor: update Login component with optimized imports, form validation, and submit functionality

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,103 +1,120 @@
+import { useState, useEffect } from "react";
 
-import news from "../assets/images/login.png"
- import { ChangeEvent,useState } from "react";
-import mainlogo from "../assets/icons/logo.svg"
-import { Form, Input } from "antd";
- import { PrimaryButton } from "./Button/PrimaryButton";
-//import { useAntdUseApp } from "../hooks/useAntdUseApp";
+import { Image, Form, Input } from "antd";
 import { UserOutlined, KeyOutlined } from "@ant-design/icons";
-import { Image } from "antd";
 
-export const Login = () => {
-    const [formData, setFormData] = useState({
-      Email: '',
-      password:''
-    });
-    const [form] = Form.useForm();
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-      const { name, value } = event.target;
-      setFormData(prevState => ({ ...prevState, [name]: value }));
-    };
-    const isFormComplete = formData.Email && formData.password;
+import { PrimaryButton } from "./Button";
 
-  return (
-    <div className=" flex h-screen">
-       <div className=" flex w-1/2 h-full ">
-        <img className=" flex object-cover h-full"  src={news} />
-      </div>
-      <div className="flex justify-center w-1/2 h-full ">
-     
+import loginImage from "../assets/images/login.png";
+import logoImage from "../assets/icons/logo.svg";
 
-      <Form
-        form={form}
-        name="LoginForm"
-        initialValues={{ remember: true }}
-       // onFinish={onFinish}
-        size="middle"
-        layout="vertical"
-        className=" flex flex-col justify-center items-center gap-5"
-        scrollToFirstError
-      >
-        {/* Page Title */}
-        <Image className="w-auto h-auto" src={mainlogo}/>
-        <h2 className="m-0 p-0 font-lato font-bold text-[28px]  text-primary  ">PEP Adverse NEWS Screening</h2>
-        <h3 className="m-0 p-0 font-lato font-bold text-[18px]  text-primary">Login</h3>
-        {/* Email */}
-        
+import { styles } from "../assets/styles";
 
-        <Form.Item
-          name="bank_POC_email"
-          label={
-            <span className="w-full text-left m-0 p-0 font-lato font-bold text-[16px] leading-[24px]  text-black">Email</span>
-          }
-          className="w-full m-0"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid email!",
-            },
-            {
-              required: true,
-              message: "Please input your email!",
-            },
-          ]}
-        >
-            
-          <Input
-            placeholder="Email*"
-            className="!bg-[transparent] [&>input]:!bg-[transparent]"
-            prefix={<UserOutlined />}
-            onChange={handleInputChange}
-            name="Email"
-          />
-        </Form.Item>
-     
-        {/* Password */}
-        <Form.Item
-          name="bank_password"
-          className="w-full m-0"
-          label={
-            <span className="w-full text-left m-0 p-0 font-lato font-bold text-[16px] leading-[24px]  text-black"> Password</span>
-          }
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password
-            placeholder="Password*"
-            className="!bg-[transparent] [&>input]:!bg-[transparent]"
-            prefix={<KeyOutlined />}
-             onChange={handleInputChange}
-            name="password"
-          />
-        </Form.Item>
-
-      <PrimaryButton htmlType="submit"  className={!isFormComplete ? "opacity-40":"opacity-100" }>Log In</PrimaryButton>
-      </Form>
-        
-      </div>
-    
-   
-    </div>
-  )
+interface LoginData {
+  email: string;
+  password: string;
 }
 
+export const Login = () => {
+  const [form] = Form.useForm();
 
+  // Submittable state for form submit button
+  const [submittable, setSubmittable] = useState<boolean>(false);
+
+  // Watch all values
+  const values = Form.useWatch([], form);
+
+  useEffect(() => {
+    form
+      .validateFields({ validateOnly: true })
+      .then(() => setSubmittable(true))
+      .catch(() => setSubmittable(false));
+  }, [form, values]);
+
+  const onFinish = () => {
+    // TODO: Implement login functionality
+  };
+
+  return (
+    <div className="w-full h-screen flex">
+      <Image
+        preview={false}
+        src={loginImage}
+        alt="Login Image"
+        height={"100%"}
+        width={"50%"}
+        className="object-cover"
+      />
+
+      <div className="w-1/2 h-full flex flex-col justify-center items-center p-10 gap-5">
+        {/* Logo */}
+        <Image preview={false} src={logoImage} alt="Logo Image" width={"30%"} />
+
+        {/* Page Title */}
+        <h2 className={styles.heading3}>PEP Adverse NEWS Screening</h2>
+        <h3 className={styles.heading4}>Login</h3>
+
+        <Form
+          form={form}
+          name="LoginForm"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          size="large"
+          layout="vertical"
+          className="w-full flex flex-col items-center gap-5"
+          scrollToFirstError
+          autoComplete="off"
+        >
+          {/* Email */}
+          <Form.Item<LoginData>
+            name="email"
+            label={<span className={styles.label}>Email</span>}
+            className="min-w-[50%] m-0"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+              {
+                type: "email",
+                message: "The input is not valid email!",
+              },
+            ]}
+          >
+            <Input
+              placeholder="Please enter your email"
+              prefix={<UserOutlined />}
+            />
+          </Form.Item>
+
+          {/* Password */}
+          <Form.Item<LoginData>
+            name="password"
+            className="min-w-[50%] m-0"
+            label={<span className={styles.label}>Password</span>}
+            rules={[
+              { required: true, message: "Please input your password!" },
+              {
+                min: 6,
+                message: "Password must be at least 6 characters long!",
+              },
+            ]}
+          >
+            <Input.Password
+              placeholder="Please enter your password"
+              prefix={<KeyOutlined />}
+            />
+          </Form.Item>
+
+          <PrimaryButton
+            htmlType="submit"
+            className={`${!submittable && styles.disabled}`}
+            disabled={!submittable}
+          >
+            Login
+          </PrimaryButton>
+        </Form>
+      </div>
+    </div>
+  );
+};
