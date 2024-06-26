70a0af0ad9ecc1def243dbd6a82a3c5e1056448a
AishaNaeem123 <i212451@nu.edu.pk>
2024-06-26 16:16:17 +0500

    refactor: added editable rows

--- src/pages/Preview/PreviewTable.tsx
+++ src/pages/Preview/PreviewTable.tsx
@@ -1,7 +1,115 @@
-import { Table } from "antd";
-import { ColumnsType } from "antd/es/table";
+import React, { useContext, useEffect, useRef, useState } from "react";
+
+import type { GetRef, InputRef } from "antd";
+import { Form, Input, Table } from "antd";
+
+type FormInstance<T> = GetRef<typeof Form<T>>;
+
+const EditableContext = React.createContext<FormInstance<unknown> | null>(null);
+
+interface Item {
+  key: string;
+  Serial: string;
+  English_Name: string;
+  Urdu_Name: string;
+  Alias_English: string;
+  Alias_Urdu: string;
+  Organization: string;
+}
+type EditableRowProps = {
+  index: number;
+} & React.HTMLAttributes<HTMLTableRowElement>;
+
+const EditableRow = (props: EditableRowProps) => {
+  const [form] = Form.useForm();
+  return (
+    <Form form={form} component={false}>
+      <EditableContext.Provider value={form}>
+        <tr {...props} />
+      </EditableContext.Provider>
+    </Form>
+  );
+};
+
+interface EditableCellProps {
+  title: React.ReactNode;
+  editable: boolean;
+  dataIndex: keyof Item;
+  record: Item;
+  handleSave: (record: Item) => void;
+}
+
+const EditableCell: React.FC<React.PropsWithChildren<EditableCellProps>> = ({
+  title,
+  editable,
+  children,
+  dataIndex,
+  record,
+  handleSave,
+  ...restProps
+}) => {
+  const [editing, setEditing] = useState(false);
+  const inputRef = useRef<InputRef>(null);
+  const form = useContext(EditableContext)!;
+
+  useEffect(() => {
+    if (editing) {
+      inputRef.current?.focus();
+    }
+  }, [editing]);
+
+  const toggleEdit = () => {
+    setEditing(!editing);
+    form.setFieldsValue({ [dataIndex]: record[dataIndex] });
+  };
+
+  const save = async () => {
+    try {
+      const values = await form.validateFields();
+      if(!values){
+        throw new Error("values not found")
+      }
+      toggleEdit();
+      handleSave({ ...record, ...values });
+    } catch (errInfo) {
+      console.log("Save failed:", errInfo);
+    }
+  };
+
+  let childNode = children;
+
+  if (editable) {
+    childNode = editing ? (
+      <Form.Item
+        style={{ margin: 0 }}
+        name={dataIndex}
+        rules={[
+          {
+            required: true,
+            message: `${title} is required.`,
+          },
+        ]}
+      >
+        <Input ref={inputRef} onPressEnter={save} onBlur={save} />
+      </Form.Item>
+    ) : (
+      <div
+        className="editable-cell-value-wrap"
+        style={{ paddingRight: 24 }}
+        onClick={toggleEdit}
+      >
+        {children}
+      </div>
+    );
+  }
+
+  return <td {...restProps}>{childNode}</td>;
+};
+
+type EditableTableProps = Parameters<typeof Table>[0];
 
 interface DataType {
+  key: React.Key;
   Serial: string;
   English_Name: string;
   Urdu_Name: string;
@@ -10,75 +118,130 @@ interface DataType {
   Organization: string;
 }
 
-const columns: ColumnsType = [
-  {
-    title: "Serial Number",
-    dataIndex: "Serial",
-    key: "Serial",
-    width: 130,
-    align: "center",
-  },
-  {
-    title: "English Name",
-    dataIndex: "English_Name",
-    key: "English_Name",
-    align: "center",
-  },
-
-  {
-    title: "Urdu Name",
-    dataIndex: "Urdu_Name",
-    key: "Urdu_Name",
-    align: "center",
-  },
-  {
-    title: "AKA(English)",
-    dataIndex: "Alias_English",
-    key: "Alias_English",
-    align: "center",
-  },
-  {
-    title: "AKA(Urdu)",
-    dataIndex: "Alias_Urdu",
-    key: "Alias_Urdu",
-    align: "center",
-  },
-
-  {
-    title: "Organization",
-    dataIndex: "Organization",
-    key: "Organization",
-    align: "center",
-  },
-];
-
-const data: DataType[] = [
-  {
-    Serial: "1",
-    English_Name: "John smith",
-    Urdu_Name: "john",
-    Alias_English: "jhonny",
-    Alias_Urdu: "jonn",
-    Organization: "PTI",
-  },
-  {
-    Serial: "2",
-    English_Name: "Imran Khan",
-    Urdu_Name: "niazi",
-    Alias_English: "Immmy",
-    Alias_Urdu: "Khan",
-    Organization: "PTI",
-  },
-  {
-    Serial: "3",
-    English_Name: "Bilawal Bhutto",
-    Urdu_Name: "Bhutto",
-    Alias_English: "BB",
-    Alias_Urdu: "Bilalwal",
-    Organization: "PPP",
-  },
-];
+type ColumnTypes = Exclude<EditableTableProps["columns"], undefined>;
 
 export const PreviewTable = () => {
-  return <Table size="middle" columns={columns} dataSource={data} />;
+  const [dataSource, setDataSource] = useState<DataType[]>([
+    {
+      key: "0",
+      Serial: "1",
+      English_Name: "John smith",
+      Urdu_Name: "john",
+      Alias_English: "jhonny",
+      Alias_Urdu: "jonn",
+      Organization: "PTI",
+    },
+    {
+      key: "1",
+      Serial: "2",
+      English_Name: "Imran Khan",
+      Urdu_Name: "niazi",
+      Alias_English: "Immmy",
+      Alias_Urdu: "Khan",
+      Organization: "PTI",
+    },
+    {
+      key: "2",
+      Serial: "3",
+      English_Name: "Bilawal Bhutto",
+      Urdu_Name: "Bhutto",
+      Alias_English: "BB",
+      Alias_Urdu: "Bilalwal",
+      Organization: "PPP",
+    },
+  ]);
+
+  const defaultColumns: (ColumnTypes[number] & {
+    editable?: boolean;
+    dataIndex: string;
+  })[] = [
+    {
+      title: "Serial Number",
+      dataIndex: "Serial",
+      key: "Serial",
+      width: 130,
+      align: "center",
+    },
+    {
+      title: "English Name",
+      dataIndex: "English_Name",
+      key: "English_Name",
+      align: "center",
+      editable: true,
+    },
+
+    {
+      title: "Urdu Name",
+      dataIndex: "Urdu_Name",
+      key: "Urdu_Name",
+      align: "center",
+      editable: true,
+    },
+    {
+      title: "AKA(English)",
+      dataIndex: "Alias_English",
+      key: "Alias_English",
+      align: "center",
+      editable: true,
+    },
+    {
+      title: "AKA(Urdu)",
+      dataIndex: "Alias_Urdu",
+      key: "Alias_Urdu",
+      align: "center",
+      editable: true,
+    },
+
+    {
+      title: "Organization",
+      dataIndex: "Organization",
+      key: "Organization",
+      align: "center",
+      editable: true,
+    },
+  ];
+
+  const handleSave = (row: DataType) => {
+    const newData = [...dataSource];
+    const index = newData.findIndex((item) => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    setDataSource(newData);
+  };
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+
+  const columns = defaultColumns.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record: DataType) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
+
+  return (
+    <Table
+      components={components}
+      size="middle"
+      rowClassName={() => "editable-row"}
+      dataSource={dataSource}
+      columns={columns as ColumnTypes}
+    />
+  );
 };
