e501b93c80648537b75740f89184c01ebfb27590
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2024-07-01 15:01:23 +0500

    feat:  added summary page layout

---
+++ src/pages/Summary/index.tsx
@@ -0,0 +1,43 @@
+import { DownloadOutlined } from "@ant-design/icons";
+import { SecondaryButton, LinkButton } from "../../components/Button";
+
+import { styles } from "../../assets/styles";
+
+export const Summary = () => {
+  return (
+    <section className={`!gap-2 ${styles.section}`}>
+      <p>
+        <span className={styles.label}>Date Range: </span>
+        05/07/2020 - 05/07/2022
+      </p>
+
+      <div className="!m-0 !p-0 flex flex-row gap-10">
+        <h2 className={styles.heading2}>
+          Pakistan minister ditched offshore plans amid ‘concerns’ over tax
+          authority
+        </h2>
+      </div>
+      <p className={styles.label}>Imran Khan Niazi</p>
+      <div className=" !m-0 !p-0 flex justify-between">
+        <p className="font-semibold text-black">Primary PEP</p>
+        <LinkButton
+          icon={<DownloadOutlined />}
+          className="text-primary font-bold"
+        >
+          Download
+        </LinkButton>
+      </div>
+
+      <div className="border-t border-slate_gray my-0 py-0"></div>
+
+      {/* add sections */}
+      <div className="flex flex-row gap-5 h-1/3 ">
+        {/* add sources wala daba */}
+        {/* add pep details wala daba */}
+      </div>
+      <div className="flex  ">{/* add news summary wala daba */}</div>
+
+      <SecondaryButton>Back</SecondaryButton>
+    </section>
+  );
+};
