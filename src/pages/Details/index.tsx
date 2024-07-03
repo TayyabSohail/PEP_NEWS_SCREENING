88d28aca3ee73b6b3504c2e7440a813c5ae31432
moinuddinrao <moinrajput594@gmail.com>
2024-07-03 12:22:48 +0500

    refactor: move languageFilter to details page

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,7 +1,9 @@
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
+
 import { DownloadOutlined } from "@ant-design/icons";
 import { SecondaryButton, LinkButton } from "../../components/Button";
 
-import { LanguageFilter } from "./languageFilter";
 import { PEPDetailsCard } from "./PEPDetails";
 import { Events } from "./Events";
 import { Sources } from "./Sources";
@@ -9,13 +11,24 @@ import { Sources } from "./Sources";
 import { styles } from "../../assets/styles";
 
 export const Details = () => {
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: "English NEWS",
+    },
+    {
+      key: "2",
+      label: "Urdu NEWS",
+    },
+  ];
+
   return (
-    <section className={`!gap-3 ${styles.section}`}>
-      <div className="!m-0 !p-0 flex flex-row gap-10">
+    <section className={`${styles.section}`}>
+      <div className="flex flex-row gap-10">
         <h2 className={styles.heading2}>01 Imran Khan Niazi</h2>
         <h2 className={styles.heading2}>عمران خان نیازی</h2>
       </div>
-      <div className=" !m-0 !p-0 flex justify-between">
+      <div className="flex justify-between">
         <p>
           <span className={styles.label}>Date Range: </span>
           05/07/2020 - 05/07/2022
@@ -29,15 +42,15 @@ export const Details = () => {
         </LinkButton>
       </div>
 
-      <div className="border-t border-slate_gray my-0 py-0"></div>
+      <span className={styles.line} />
 
       {/* Add language filter */}
-      <LanguageFilter />
+      <Tabs defaultActiveKey="1" items={items} />
       <div className="flex flex-row gap-10">
-        <div className=" w-2/3">
+        <div className="w-2/3">
           <Events />
         </div>
-        <div className="flex flex-col gap-5">
+        <div className="w-1/3 flex flex-col gap-10">
           <PEPDetailsCard />
           <Sources />
         </div>
