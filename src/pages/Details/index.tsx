d83a078424c36b026ce80fe6a94f88e445977811
moinuddinrao <moinrajput594@gmail.com>
2024-07-04 13:16:01 +0500

    refactor: use Tab for language filter for English and Urdu news

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -15,10 +15,32 @@ export const Details = () => {
     {
       key: "1",
       label: "English NEWS",
+      children: (
+        <div className="mt-5 flex gap-10">
+          <div className="w-2/3">
+            <NewsEvents />
+          </div>
+          <div className="w-1/3 flex flex-col gap-10">
+            <PEPDetails />
+            <Sources />
+          </div>
+        </div>
+      ),
     },
     {
       key: "2",
       label: "Urdu NEWS",
+      children: (
+        <div className="mt-5  flex flex-row gap-10">
+          <div className="w-2/3">
+            <NewsEvents />
+          </div>
+          <div className="w-1/3 flex flex-col gap-10">
+            <PEPDetails />
+            <Sources />
+          </div>
+        </div>
+      ),
     },
   ];
 
@@ -44,19 +66,8 @@ export const Details = () => {
 
       <span className={styles.line} />
 
-      {/* Add language filter */}
+      {/* English and Urdu News */}
       <Tabs defaultActiveKey="1" items={items} />
-      <div className="flex flex-row gap-10">
-        <div className="w-2/3">
-          <NewsEvents />
-        </div>
-        <div className="w-1/3 flex flex-col gap-10">
-          <PEPDetails />
-          <Sources />
-        </div>
-      </div>
-
-      {/* add sections */}
 
       <SecondaryButton>Back</SecondaryButton>
     </section>
