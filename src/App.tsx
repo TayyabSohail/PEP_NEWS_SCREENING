6ec0a7fb6f8c4cccb93a017cfbb80da272fc71bb
moinuddinrao <moinrajput594@gmail.com>
2024-07-08 11:06:12 +0500

    feat: setup react-query and react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,40 @@
 import { App as AntdApp, ConfigProvider } from "antd";
 
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { Router } from "./Router";
+
+import { queryClient } from "./utils/react-query.service";
 
 export const App = () => {
   return (
-    <AntdApp>
-      <div className="App">
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: "#085FAB",
-              colorPrimaryBg: "#E5EDFF",
-              colorBorder: "#085FAB",
-              borderRadius: 6,
-              colorText: "#454545",
-              colorTextSecondary: "#454545",
-            },
-            components: {
-              Table: {
-                headerBg: "#FFFFFF",
-                headerColor: "#085FAB",
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <AntdApp>
+        <div className="App">
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: "#085FAB",
+                colorPrimaryBg: "#E5EDFF",
+                colorBorder: "#085FAB",
+                borderRadius: 6,
+                colorText: "#454545",
+                colorTextSecondary: "#454545",
               },
-            },
-          }}
-        >
-          <Router />
-        </ConfigProvider>
-      </div>
-    </AntdApp>
+              components: {
+                Table: {
+                  headerBg: "#FFFFFF",
+                  headerColor: "#085FAB",
+                },
+              },
+            }}
+          >
+            <Router />
+          </ConfigProvider>
+        </div>
+      </AntdApp>
+    </QueryClientProvider>
   );
 };
