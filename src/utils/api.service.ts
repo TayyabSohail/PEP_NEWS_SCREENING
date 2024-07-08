d79c726fa2ab99e0bc7e8c2f45f9351cda38cb21
moinuddinrao <moinrajput594@gmail.com>
2024-07-08 12:06:44 +0500

    feat: setup axios in api.service.ts

--- src/utils/api.service.ts
+++ src/utils/api.service.ts
@@ -1,16 +1,33 @@
-// import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
-// import {
-//     useQuery,
-//     useMutation,
-//     useQueryClient,
-//     QueryClient,
-//     QueryClientProvider,
-//   } from 'react-query'
+import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
 
-// const queryClient = new QueryClient()
+export const endpoints = {
+  auth: {
+    signup: "/auth/signup",
+    login: "/auth/login",
+  },
+};
 
-// function App() {
-//     return(
-        
-//     )
-// }
+const apiService = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+});
+
+export const GET = <Response>(url: string) => apiService.get<Response>(url);
+
+export const PUT = <Request, Response>(url: string, body: Request) =>
+  apiService.put<Request, AxiosResponse<Response>, Request>(url, body);
+
+export const POST = <Request, Response>(
+  url: string,
+  body: Request,
+  config?: AxiosRequestConfig
+) =>
+  apiService.post<Request, AxiosResponse<Response>, Request>(url, body, config);
+
+export const PATCH = <Request, Response>(url: string, body: Request) =>
+  apiService.patch<Request, AxiosResponse<Response>, Request>(url, body);
+
+export const DELETE = <Response>(url: string) =>
+  apiService.delete<Response>(url);
