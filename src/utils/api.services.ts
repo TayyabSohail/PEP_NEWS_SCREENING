aaacdf30a9b71c84f82fac4cc36d245b09dcdcef
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2024-07-08 10:45:27 +0500

    refector: created utils folder for api

---
+++ src/utils/api.services.ts
@@ -0,0 +1,16 @@
+// import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+// import {
+//     useQuery,
+//     useMutation,
+//     useQueryClient,
+//     QueryClient,
+//     QueryClientProvider,
+//   } from 'react-query'
+
+// const queryClient = new QueryClient()
+
+// function App() {
+//     return(
+        
+//     )
+// }
