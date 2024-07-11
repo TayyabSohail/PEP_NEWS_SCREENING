16a7d1e954f732772d6dc27a25d8bbb8ff2576b7
Tayyab Sohail <m.tayyabsohail614@gmail.com>
2024-07-11 11:30:52 +0500

    feat: interface for ResponseData

--- src/api/result.api.ts
+++ src/api/result.api.ts
@@ -3,39 +3,85 @@ import { AxiosError } from "axios";
 import { useAppProps } from "antd/es/app/context";
 import { endpoints, POST } from "../utils/api.service";
 
-import { ROUTES } from "../constants/routes";
-
 export interface RequestData {
   startDate: string;
   endDate: string;
-  dataSet: [
-    {
-      ID: number;
-      englishName: string;
-      urduName: string;
-      akaEnglish: string;
-      akaUrdu: string;
-      organizations: string;
-      designations: string;
-      relationships: string;
-      primarySecondary: string;
-      keywords1: string;
-      keywords2: string;
-      keywords3: string;
-      keywords4: string;
-      keywords5: string;
-    }
-  ];
+  dataSet: {
+    ID: number;
+    englishName: string;
+    urduName: string;
+    akaEnglish: string;
+    akaUrdu: string;
+    organizations: string;
+    designations: string;
+    relationships: string;
+    primarySecondary: string;
+    keywords1: string;
+    keywords2: string;
+    keywords3: string;
+    keywords4: string;
+    keywords5: string;
+  }[];
+}
+[];
+
+type EVENTS_TYPE = "urduEvents" | "engEvents";
+
+interface Events {
+  item: {
+    sources: string[];
+    urls: string[];
+    descriptions: string[];
+    _id: string;
+    eventName: string;
+    dte: string;
+    sentiments: string;
+  }[];
+
+  record: {
+    negativeSentiments: number;
+    positiveSentiments: number;
+    neturalsentSentiments: number;
+    Events: number;
+  }[];
+
+  OriginalKeyword: string;
+}
+[];
+export interface ResponseData {
+  success: boolean;
+  message: string;
+  data: Record<EVENTS_TYPE, Events>;
 }
 
-export interface ResponseData {}
+interface ResultProps extends RequestData {
+  notification: useAppProps;
+}
 
-export const result = async ({ startDate, endDate, dataSet }: RequestData) => {
+export const result = async ({
+  startDate,
+  endDate,
+  dataSet,
+  notification,
+}: ResultProps) => {
+  console.log({ startDate, endDate, dataSet });
   POST<RequestData, ResponseData>(endpoints.result, {
     startDate,
     endDate,
     dataSet,
   })
-    .then(({}) => {})
-    .catch((error: AxiosError) => {});
+    .then(({ data }) => {
+      notification?.notification.success({
+        message: "Record Found",
+      });
+      console.log(data.success);
+    })
+    .catch((error: AxiosError) => {
+      console.error("Error occurred:", error);
+      notification?.notification.error({
+        message: "Record not Found",
+      });
+
+      return error.response?.data;
+    });
 };
